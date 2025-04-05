addba6b9fc3017b64b9f8c97c71ea375c4696acb
SoeYeong <77740000+SoeYeong@users.noreply.github.com>
2025-04-06 05:36:08 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250406/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#define MAX 16
+using namespace std;
+
+int N;
+int work[MAX][2];
+int ans = 0;
+
+void dfs(int idx, int val) {
+    if (idx > N) {
+        return;
+    }
+    if (idx == N) {
+        ans = (val > ans) ? val : ans;
+        return;
+    }
+
+    for (int i = idx + 1; i <= N; i++) {
+        dfs(i + work[i][0] - 1, val + work[i][1]);
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // input
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> work[i][0] >> work[i][1];
+    }
+    
+    dfs(0, 0);
+    cout << ans;
+
+    return 0;
+}
