3176351c8b08e6d3fa81c9d8f7a71d960197814c
SoeYeong <77740000+SoeYeong@users.noreply.github.com>
2025-04-06 04:54:25 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250406/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define MAX 1000001
+using namespace std;
+
+int N;
+int store[MAX];
+int tester[2];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // input
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> store[i];
+    }
+    cin >> tester[0] >> tester[1];
+
+    // solve
+    long long ans = 0;
+    for (int i = 0; i < N; i++) {
+        ans += 1;
+        if (store[i] / tester[0] == 0) {
+            continue;
+        }
+        store[i] -= tester[0];
+        ans += (long long)(store[i] - 1) / tester[1] + 1;
+    }
+
+    cout << ans;
+
+    return 0;
+}
