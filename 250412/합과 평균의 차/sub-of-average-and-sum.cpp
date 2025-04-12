e80ef368772ccf19dfa4cf4b1541b2f9bfa9c36a
SoeYeong <77740000+SoeYeong@users.noreply.github.com>
2025-04-12 23:44:04 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250412/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a>>b>>c;
+    
+    int sum = a+b+c;
+    cout << sum <<"\n" << sum/3 << "\n" << sum-sum/3;
+    return 0;
+}
